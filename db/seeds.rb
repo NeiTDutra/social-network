a3bf45963dcdc065bd6dc5d4517961c74a25504c
nei <nei.thomass@gmail.com>
2021-06-26 07:59:57 -0300

    instalando gem bcrypt

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Admin.create([{email: "admin@admin.com", password: "123456"},{email: "fake@fake.com", password: "fake2021"}])
+Admin.create(name: "Admin", email: "adm@adm.com", password: "123456", password_confirmation: "123456")
 puts "admin created now"
